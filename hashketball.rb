cafc9c9fdc59f23555bdb258348c7f0fcb02b251
T Benson <t.benson@live.co.uk>
2019-12-09 22:49:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -220,8 +220,6 @@ def player_numbers(team)
       stats[:players].each { |person|
         team_numbers << person[:number]
       }
-    # else 
-    #   return "That team wasn't playing in this game"
     end
   end
   team_numbers
